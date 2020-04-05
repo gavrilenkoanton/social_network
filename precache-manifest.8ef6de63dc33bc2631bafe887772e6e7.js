3f37d69a3d528a0ed961be358af406326b2aabef
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-04-05 19:16:50 +0300

    Updates

---
+++ precache-manifest.8ef6de63dc33bc2631bafe887772e6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1272fb5eb61eb502a7187750ff9c0b3",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "d8b8ff8c82c17460c8a6",
+    "url": "/social_network/static/css/main.3a8ff806.chunk.css"
+  },
+  {
+    "revision": "03732c33f00b078014da",
+    "url": "/social_network/static/js/2.10e4ed29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.10e4ed29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b8ff8c82c17460c8a6",
+    "url": "/social_network/static/js/main.96f7dc82.chunk.js"
+  },
+  {
+    "revision": "8f624a9611af6896beea",
+    "url": "/social_network/static/js/runtime-main.bb462cd5.js"
+  }
+]);
