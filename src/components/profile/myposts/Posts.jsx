7923c10611aade27e57645c46445fe0f57d96d87
anton <gavrilenko7732@gmail.com>
2020-06-04 11:50:14 +0300

    form validation added

--- src/components/profile/myposts/Posts.jsx
+++ src/components/profile/myposts/Posts.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Posts.module.css'
 import Post from "./post/Post";
 import {Field, reduxForm} from "redux-form";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {Textarea} from "../../common/FormControl/FormControl";
 
 function Posts(props) {
 
@@ -22,11 +24,13 @@ function Posts(props) {
     );
 }
 
+const maxLength10 = maxLengthCreator(10);
 
 let AddPostForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
-            <Field component={"textarea"} name={"newPostBody"} placeholder={"Enter your post"}/>
+            <Field component={Textarea} name={"newPostBody"} placeholder={"Enter your post"}
+                   validate={[required, maxLength10]}/>
             <button>send post</button>
         </form>
     )
